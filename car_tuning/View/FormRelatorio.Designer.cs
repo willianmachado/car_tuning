cb2e1ab7b4b9eb54bbe112e3f2d44adec9a108ae
willianmachado <wasmachado@gmail.com>
2018-10-28 22:36:42 -0300

    relatorios

--- car_tuning/View/FormRelatorio.Designer.cs
+++ car_tuning/View/FormRelatorio.Designer.cs
@@ -28,32 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ambiance_TabControl1 = new Ambiance.Ambiance_TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.ambiance_TabControl1 = new Ambiance.Ambiance_TabControl();
             this.ambiance_TabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ambiance_TabControl1
+            // tabPage6
             // 
-            this.ambiance_TabControl1.Controls.Add(this.tabPage1);
-            this.ambiance_TabControl1.Controls.Add(this.tabPage2);
-            this.ambiance_TabControl1.ItemSize = new System.Drawing.Size(80, 24);
-            this.ambiance_TabControl1.Location = new System.Drawing.Point(0, 0);
-            this.ambiance_TabControl1.Name = "ambiance_TabControl1";
-            this.ambiance_TabControl1.SelectedIndex = 0;
-            this.ambiance_TabControl1.Size = new System.Drawing.Size(1244, 675);
-            this.ambiance_TabControl1.TabIndex = 0;
+            this.tabPage6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.tabPage6.Location = new System.Drawing.Point(4, 28);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1236, 643);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Rank ";
             // 
-            // tabPage1
+            // tabPage5
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
-            this.tabPage1.Location = new System.Drawing.Point(4, 28);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1236, 643);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.tabPage5.Location = new System.Drawing.Point(4, 28);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1236, 643);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Funcionário";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.tabPage4.Location = new System.Drawing.Point(4, 28);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1236, 643);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Serviços";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1236, 643);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Peças";
             // 
             // tabPage2
             // 
@@ -63,7 +86,32 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1236, 643);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Carros";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1236, 643);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Clientes";
+            // 
+            // ambiance_TabControl1
+            // 
+            this.ambiance_TabControl1.Controls.Add(this.tabPage1);
+            this.ambiance_TabControl1.Controls.Add(this.tabPage2);
+            this.ambiance_TabControl1.Controls.Add(this.tabPage3);
+            this.ambiance_TabControl1.Controls.Add(this.tabPage4);
+            this.ambiance_TabControl1.Controls.Add(this.tabPage5);
+            this.ambiance_TabControl1.Controls.Add(this.tabPage6);
+            this.ambiance_TabControl1.ItemSize = new System.Drawing.Size(80, 24);
+            this.ambiance_TabControl1.Location = new System.Drawing.Point(0, 0);
+            this.ambiance_TabControl1.Name = "ambiance_TabControl1";
+            this.ambiance_TabControl1.SelectedIndex = 0;
+            this.ambiance_TabControl1.Size = new System.Drawing.Size(1244, 675);
+            this.ambiance_TabControl1.TabIndex = 0;
             // 
             // FormRelatorio
             // 
@@ -81,8 +129,12 @@
 
         #endregion
 
-        private Ambiance.Ambiance_TabControl ambiance_TabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Ambiance.Ambiance_TabControl ambiance_TabControl1;
     }
 }
